84393eef3e53b0a28fc4b6d2c02283ef30503444
Durgaprasad <durgaprasadptr10@gmail.com>
2022-12-30 20:44:14 +0530

    First Commit

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TypingBox from '../Components/TypingBox'
+
+const HomePage = () => {
+    return (
+        <div className='canvas'>
+            <TypingBox />
+        </div>
+    )
+}
+
+export default HomePage
